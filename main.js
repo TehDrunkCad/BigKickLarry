93f3a636ca228941414283f81f1078c3916e8c76
BigKickLarry <leahyfedele123@gmail.com>
2018-09-01 00:58:55 -0400

    Solar System
    Changed HemisphericLight to PointLight
    
    Added planets with rough sizing.

--- main.js
+++ main.js
@@ -15,11 +15,49 @@
                         camera.attachControl(canvas, true);                        
                         
                         // Add light to the scene
-                        var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);              
+                        var light1 = new BABYLON.PointLight("light1", new BABYLON.Vector3(0, 0, 0), scene);              
 
                         // Add and manipulate meshes in the scene
-                        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
-                        let torus = BABYLON.MeshBuilder.CreateTorus("torus",{diameter:5}, scene);
+                        let Sun = BABYLON.MeshBuilder.CreateSphere("Sun",{diameter: 15}, scene);
+                        
+                        let Mercury = BABYLON.MeshBuilder.CreateSphere("Mercury",{diameter:2}, scene);
+                        Mercury.position.x = 10
+
+                        let Venus = BABYLON.MeshBuilder.CreateSphere("Venus",{diameter: 2.5}, scene);
+                        Venus.position.x = 25
+
+                        let Earth = BABYLON.MeshBuilder.CreateSphere("Earth",{diameter:3}, scene);
+                        Earth.position.x = 40
+
+                        let Earthmoon = BABYLON.MeshBuilder.CreateSphere("Earthmoon",{diameter:1}, scene);
+                        Earthmoon.position.x = 43
+                        Earthmoon.position.y = 1
+
+                        let Mars = BABYLON.MeshBuilder.CreateSphere("Mars",{diameter:1.8}, scene);
+                        Mars.position.x = 55
+
+                        let Jupiter = BABYLON.MeshBuilder.CreateSphere("Jupiter",{diameter:7}, scene);
+                        Jupiter.position.x = 70
+
+                        var Saturn = BABYLON.MeshBuilder.CreateSphere("Saturn", {diameter:5}, scene);
+                        Saturn.position.x = 85
+                        let Saturnring = BABYLON.MeshBuilder.CreateTorus("Saturnring",{diameter:10}, scene);
+                        Saturnring.position.x = 85
+
+                        let Uranus = BABYLON.MeshBuilder.CreateSphere("Uranus",{diameter:5.5}, scene);
+                        Uranus.position.x = 100
+
+                        let Neptune = BABYLON.MeshBuilder.CreateSphere("Neptune",{diameter:5}, scene);
+                        Neptune.position.x = 115
+
+                        
+                        
+                        
+                        
+                        
+
+
+                        
 
                         return scene;
                 };
