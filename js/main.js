6f06826b4947050f2296d8d258baed7d26d91583
Unknown <leahyfedele123@gmail.com>
2019-02-14 00:30:55 -0500

    ChangeVariable/SetParent/AddRotation
    Set all variables to lowercase, set sun as parent to planet meshes, added rotation to earth and sun

--- js/main.js
+++ js/main.js
@@ -22,48 +22,59 @@
 
                         // Add and manipulate meshes in the scene
                         
-                        let Sun = BABYLON.MeshBuilder.CreateSphere("Sun",{diameter: 15}, scene);                       
-                        var Suncolor = new BABYLON.StandardMaterial("Suncolor", scene);
-                        Suncolor.alpha = 0.99;//**alpha on material
-                        Suncolor.emissiveColor = new BABYLON.Color3(1, 1, 0);
-                        Sun.material = Suncolor;
-                        Sun.position.x = 20
+                        let sun = BABYLON.MeshBuilder.CreateSphere("sun",{diameter: 15}, scene);                       
+                        var suncolor = new BABYLON.StandardMaterial("suncolor", scene);
+                        suncolor.alpha = 0.99;//**alpha on material
+                        suncolor.emissiveColor = new BABYLON.Color3(1, 1, 0);
+                        sun.material = suncolor;
                         
-                        let Mercury = BABYLON.MeshBuilder.CreateSphere("Mercury",{diameter:2}, scene);
-                        Mercury.position.x = 10
-
-                        let Venus = BABYLON.MeshBuilder.CreateSphere("Venus",{diameter: 2.5}, scene);
-                        Venus.position.x = 25
-
-                        let Earth = BABYLON.MeshBuilder.CreateSphere("Earth",{diameter:3}, scene);
-                        Earth.position.x = 40
-                        Earthtexture = new BABYLON.StandardMaterial("Earthtexture", scene);
-                        Earthtexture.ambientTexture = new BABYLON.Texture("/public/earth_texture.jpg", scene); // How do I give BABYLON a path to saved .png file? Or how do I upload it into its database?
-                        Earth.material = Earthtexture
-
-                        let Earthmoon = BABYLON.MeshBuilder.CreateSphere("Earthmoon",{diameter:1}, scene);
-                        Earthmoon.position.x = 43
-                        Earthmoon.position.y = 1
-
-                        let Mars = BABYLON.MeshBuilder.CreateSphere("Mars",{diameter:1.8}, scene);
-                        Mars.position.x = 55
-                        Mars.parent = Sun
-
-                        let Jupiter = BABYLON.MeshBuilder.CreateSphere("Jupiter",{diameter:7}, scene);
-                        Jupiter.position.x = 70
-
-                        var Saturn = BABYLON.MeshBuilder.CreateSphere("Saturn", {diameter:5}, scene);
-                        Saturn.position.x = 85
-                        let Saturnring = BABYLON.MeshBuilder.CreateTorus("Saturnring",{diameter:10}, scene);
-                        Saturnring.position.x = 85
-
-                        let Uranus = BABYLON.MeshBuilder.CreateSphere("Uranus",{diameter:5.5}, scene);
-                        Uranus.position.x = 100
-
-                        let Neptune = BABYLON.MeshBuilder.CreateSphere("Neptune",{diameter:5}, scene);
-                        Neptune.position.x = 115   
                         
-                        var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+                        let mercury = BABYLON.MeshBuilder.CreateSphere("mercury",{diameter:2}, scene);
+                        mercury.position.x = 10
+                        mercury.parent = sun
+
+                        let venus = BABYLON.MeshBuilder.CreateSphere("venus",{diameter: 2.5}, scene);
+                        venus.position.x = 25
+                        venus.parent = sun
+
+
+                        let earth = BABYLON.MeshBuilder.CreateSphere("earth",{diameter:3}, scene);
+                        earth.position.x = 40
+                       // earth.rotation.y = Math.PY/2; ERROR
+                        earth.parent = sun
+                        earthtexture = new BABYLON.StandardMaterial("earthtexture", scene);
+                        earthtexture.ambientTexture = new BABYLON.Texture("/public/earth_texture.jpg", scene); 
+                        earth.material = earthtexture
+
+                        let earthmoon = BABYLON.MeshBuilder.CreateSphere("earthmoon",{diameter:1}, scene);
+                        earthmoon.position.x = 3
+                        earthmoon.position.y = 1
+                        earthmoon.parent = earth
+
+                        let mars = BABYLON.MeshBuilder.CreateSphere("mars",{diameter:1.8}, scene);
+                        mars.position.x = 55
+                        mars.parent = sun
+
+                        let jupiter = BABYLON.MeshBuilder.CreateSphere("jupiter",{diameter:7}, scene);
+                        jupiter.position.x = 70
+                        jupiter.parent = sun
+
+                        var saturn = BABYLON.MeshBuilder.CreateSphere("saturn", {diameter:5}, scene);
+                        saturn.position.x = 85
+                        saturn.parent = sun
+                        let saturnring = BABYLON.MeshBuilder.CreateTorus("saturnring",{diameter:10}, scene);
+                        saturnring.position.x = 0
+                        saturnring.parent = saturn
+
+                        let uranus = BABYLON.MeshBuilder.CreateSphere("uranus",{diameter:5.5}, scene);
+                        uranus.position.x = 100
+                        uranus.parent = sun
+
+                        let neptune = BABYLON.MeshBuilder.CreateSphere("neptune",{diameter:5}, scene);
+                        neptune.position.x = 115  
+                        neptune.parent = sun 
+                        
+                        var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:2000.0}, scene);
                         var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
                         skyboxMaterial.backFaceCulling = false;
                         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("/public/textures/skybox", scene);
@@ -71,6 +82,13 @@
                         skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
                         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
                         skybox.material = skyboxMaterial;
+                        
+
+                         //Conflict with parent 'sun' and the rest of the meshes and earth moon with parent earth
+                        scene.registerBeforeRender(function () {
+                                earth.rotation.y += 0.001;
+                                //sun.rotation.y += 0.001;
+                            });
                                                 
 
                         return scene;
